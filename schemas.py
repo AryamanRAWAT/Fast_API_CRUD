e32fadb10cf13e8ba7e973a417377979b7acd639
Aryaman <arawat.kol@gmail.com>
2024-02-22 17:57:28 +0530

    added validation methods for email and id

--- schemas.py
+++ schemas.py
@@ -1,5 +1,5 @@
 #  from pydantic
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, EmailStr, validator
 import pydantic
 from typing import List, Optional, TypeVar, Generic
 T = TypeVar('T')
@@ -13,9 +13,14 @@ class UserSchema(BaseModel):
 	city : str
 	state : str
 	zip : int
-	email : str
+	email : EmailStr
 	web : Optional[str] = None
 
+	@validator('id')
+	def valid_id(cls, value):
+		if value <= 0:
+			raise ValueError(f"id must be positive: {value}")
+		return value	
 	class Config:
 		orm_mode = True
 
